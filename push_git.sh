13eeecefec1e8c20c80922ee045771477dd61297
Mariusz Sabath <mrsabath@gmail.com>
2019-10-03 18:56:23 +0200

    Delete push_git.sh

--- push_git.sh
+++
@@ -1,6 +0,0 @@
-cd "/Users/sabath/workspace/mrsabath/mytechnotes"
-git add *
-git status
-MSG=$(date +'%Y-%m-%d %H:%M:%S')
-git commit  -m "Auto-committed at $MSG"
-HOME=/Users/sabath git push origin master
